d71cb01fd37ddf87d39f99c8197592d4bdfcc191
Alexandre Pereira <xandedrop2@gmail.com>
2024-01-18 17:50:58 -0300

    alterações basicas

---
+++ src/Components/Carrossel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Carrossel.css"
+
+const Carrossel = () => {
+  return (
+    <div className='carousel'>
+        <div className="item">
+
+        </div>
+        <div className="descricao">
+
+        </div>
+    </div>
+  )
+}
+
+export default Carrossel
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='home'>
       <div className='principal'>
-        <img className="foto-PSIFlavia" src="foto-PSIFlávia.jpeg" alt="Foto da psicologa Flávia Luiza" />
+        <img className="foto-PSIFlavia" src="logopsi.png" alt="Foto da psicologa Flávia Luiza" />
         <div>
           <span></span>
           <div>
@@ -13,14 +13,20 @@ const Home = () => {
             <p className='crp'>CRP 17/7097</p>
           </div>
           <h2 className='descricao'>Psicóloga com atuação em terapia cognitiva comportamental(TCC)</h2>
+          <h3>Atendimento para adolescentes e adultos consultas online</h3>
         </div>
       </div>
-      <p className='faixa-1'>Atendimento para adolescentes e adultos consultas online</p>
       <div className='pergunta-tcc'>
         <h3 >Como a TCC pode te ajudar?</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!</p>
+        <p className='texto-explicativo'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!</p>
       </div>
-
+      <p>Carrossel de doenças</p>
+      <div className="faixa">
+        <p style={{color: "#ffffff"}}>AGENDAR MINHA CONSULTA</p>
+      </div>
+      <div className="apresentacao">
+        <h3>Quem sou eu?</h3>
+      </div>
     </div>
   )
 }
