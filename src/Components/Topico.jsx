e3379beece5886e838aa89a221c09b0d3b367a94
Alexandre Pereira <xandedrop2@gmail.com>
2024-01-23 17:26:00 -0300

    incluindo blibiotecas de animação

--- src/Components/Topico.jsx
+++ src/Components/Topico.jsx
@@ -4,8 +4,8 @@ import "./Topico.css"
 const Topico = ({className, titulo, texto}) => {
   return (
     <div className={className}>
-        <div className='titulo-topico'>{titulo}</div>
-        <div className='texto-explicativo'>{texto}</div>
+        <div className='titulo-topico' data-aos="fade-down" data-aos-duration="800">{titulo}</div>
+        <div className='texto-explicativo' data-aos="fade-up" data-aos-duration="800">{texto}</div>
     </div>
     
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,13 +7,13 @@ import Card from '../Components/Card'
 
 const Home = () => {
 
-  const servicos = 
-  [
-    "Terapia online",
-    "Psicoterapia individual",
-    "Orientação profissional",
-    "Palestras e Workshops"
-  ]
+  const servicos =
+    [
+      "Terapia online",
+      "Psicoterapia individual",
+      "Orientação profissional",
+      "Palestras e Workshops"
+    ]
 
   return (
     <div className='home'>
@@ -21,7 +21,7 @@ const Home = () => {
         <img className="foto-PSIFlavia" src="logopsi.png" alt="Foto da psicologa Flávia Luiza" />
         <div>
           <span></span>
-          <div>
+          <div >
             <p className='nome'>Flávia Luiza</p>
             <p className='crp'>CRP 17/7097</p>
           </div>
@@ -29,45 +29,56 @@ const Home = () => {
           <p className='descricao-2'>Atendimento para adolescentes e adultos consultas online</p>
         </div>
       </div>
-      <Topico 
+      <Topico
         className={'pergunta'}
-        titulo={'Como a TCC pode te ajudar?'} 
-        texto={'A Terapia Cognitivo-Comportamental (TCC) oferece uma abordagem pragmática e eficaz para ajudar indivíduos a enfrentar desafios emocionais e comportamentais. Ao identificar padrões de pensamento negativos, a TCC capacita os clientes a desafiá-los e substituí-los por pensamentos mais realistas. Além disso, a terapia concentra-se na modificação de comportamentos disfuncionais, no desenvolvimento de habilidades de enfrentamento e na promoção da autorreflexão. Estabelecer metas específicas, melhorar a regulação emocional e prevenir recaídas são aspectos-chave, destacando a natureza colaborativa entre terapeuta e cliente. A TCC visa não apenas tratar problemas presentes, mas também equipar os indivíduos com ferramentas duradouras para a gestão eficaz de desafios futuros.'} 
+        titulo={'Como a TCC pode te ajudar?'}
+        texto={'A Terapia Cognitivo-Comportamental (TCC) oferece uma abordagem pragmática e eficaz para ajudar indivíduos a enfrentar desafios emocionais e comportamentais. Ao identificar padrões de pensamento negativos, a TCC capacita os clientes a desafiá-los e substituí-los por pensamentos mais realistas. Além disso, a terapia concentra-se na modificação de comportamentos disfuncionais, no desenvolvimento de habilidades de enfrentamento e na promoção da autorreflexão. Estabelecer metas específicas, melhorar a regulação emocional e prevenir recaídas são aspectos-chave, destacando a natureza colaborativa entre terapeuta e cliente. A TCC visa não apenas tratar problemas presentes, mas também equipar os indivíduos com ferramentas duradouras para a gestão eficaz de desafios futuros.'}
       />
       <Carrossel card={
-      <Card titulo={"Ansiedade1"} descricao={"A ansiedade é algo natural de nós seres humanos. Porém em excesso se torna preocupante, quando nos incapacita em algumas questões da vida e em alguns casos acaba afetando a maneira como se vive!"} />}
+        <Card titulo={"Ansiedade1"} descricao={"A ansiedade é algo natural de nós seres humanos. Porém em excesso se torna preocupante, quando nos incapacita em algumas questões da vida e em alguns casos acaba afetando a maneira como se vive!"} />}
       />
       <div className="faixa">
-        <p style={{color: "#ffffff"}}>AGENDAR MINHA CONSULTA</p>
+        <p style={{ color: "#ffffff" }}>AGENDAR MINHA CONSULTA</p>
       </div>
-      <Topico 
+      <Topico
         className={"pergunta"}
-        titulo={'Quem sou eu?'} 
-        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'} />
+        titulo={'Quem sou eu?'}
+        texto={<div>
+          <p>
+          Me chamo Flávia Luiza, tenho 23 anos, sou Psicóloga clínica com o trabalho baseado na Terapia Cognitivo Comportamental (TCC). Meu objetivo é colaborar ativamente com você,  para entender, desafiar e modificar padrões disfuncionais com o papel de guia e facilitador e juntos iremos desenvolver estratégias práticas de enfrentamento e metas realistas. Promovendo assim, mudanças positivas e melhorando o bem-estar emocional.
+          </p>
+          
+          <p>Atendimento voltado para adolescentes e adultos de forma online.</p>
+          
+          <p>Durante minha trajetória profissional, enfrentei desafios acadêmicos, pude ter ricas experiências no manejo de situações relacionadas à ansiedade, fui suporte em processos de luto, promovi reflexões sobre a importância de uma rede de apoio e autoconhecimento e trabalhei diretamente com indivíduos diagnosticados com autismo e TDAH.</p>
+          
+          <p>Iremos trabalhar juntos para entender os problemas, encontrar maneiras práticas de resolvê-los e criar habilidades para saber lidar com situações dificeis.</p>
+        </div>
+        } />
       <Topico
         className={"servicos"}
-        titulo={'Serviços oferecidos'} 
-        texto={<Lista lista={servicos}/>} 
+        titulo={'Serviços oferecidos'}
+        texto={<Lista lista={servicos} />}
       />
-      <Topico 
+      <Topico
         className={"pergunta"}
-        titulo={'Minhas especialidades'} 
-        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'} 
+        titulo={'Minhas especialidades'}
+        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'}
       />
-      <Topico 
+      <Topico
         className={"pergunta"}
-        titulo={'Tudo sobre a terapia'} 
-        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'} 
+        titulo={'Tudo sobre a terapia'}
+        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'}
       />
-      <Topico 
+      <Topico
         className={"pergunta"}
-        titulo={'Duvidas frequentes'} 
-        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'} 
+        titulo={'Duvidas frequentes'}
+        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'}
       />
-      <Topico 
+      <Topico
         className={'pergunta'}
-        titulo={'Dias de atendimento'} 
-        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'} 
+        titulo={'Dias de atendimento'}
+        texto={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,'}
       />
       <p>Agendar minha consulta</p>
     </div>
